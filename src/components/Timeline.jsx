ba85281387a6a77f15e0f85011206a94801b8b7f
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-07-07 22:15:55 +0100

    initial timeline component

---
+++ src/components/Timeline.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {
+	VerticalTimeline,
+	VerticalTimelineElement,
+} from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component';
+
+function Timeline() {
+	return <div>Timeline</div>;
+}
+
+export default Timeline;
