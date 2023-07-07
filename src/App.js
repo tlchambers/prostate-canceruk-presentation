b63dd9ebdf626a72fa1680ec57ee985c6d8f19f3
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-07-07 23:47:32 +0100

    added timeline component to app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Introduction from './components/Introduction';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import Timeline from './components/Timeline';
 
 function App() {
 	return (
@@ -9,6 +10,7 @@ function App() {
 			<Navbar />
 			<Hero />
 			<Introduction />
+			<Timeline />
 		</div>
 	);
 }
