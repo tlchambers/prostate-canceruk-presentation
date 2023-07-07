41767a49ea4d01eacb112b909dc68b8ba7e0b1c2
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-07-07 22:15:46 +0100

    initial array - data for timeline

---
+++ src/timelineElements.js
@@ -0,0 +1,30 @@
+let timelineElements = [
+	{
+		id: 1,
+		title: 'Senior Buyers Assistant',
+		description: 'bladhfdhf',
+		date: 'Sep 2015 - Jul 2019',
+		icon: 'work',
+	},
+	{
+		id: 2,
+		title: 'fsjfsj',
+		description: 'bladhfdhf',
+		date: 'Sep 2015 - Jul 2019',
+		icon: 'work',
+	},
+	{
+		id: 3,
+		title: 'fsjfsj',
+		description: 'bladhfdhf',
+		date: 'Sep 2015 - Jul 2019',
+		icon: 'work',
+	},
+	{
+		id: 4,
+		title: 'fsjfsj',
+		description: 'bladhfdhf',
+		date: 'Sep 2015 - Jul 2019',
+		icon: 'work',
+	},
+];
