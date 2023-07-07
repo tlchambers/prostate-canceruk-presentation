db1fd695898c7901e62b0475d531ca5379ade188
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-07-07 18:24:42 +0100

    added text to intro

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,9 +3,10 @@ import React from 'react';
 function Hero() {
 	return (
 		<div className="bg-white h-screen flex flex-col justify-center items-center">
-			<h1 className="lg:text-7xl md:text-5xl sm:text-5xl text-3xl mb-14">
-				Hello There
+			<h1 className="lg:text-7xl md:text-5xl sm:text-5xl text-3xl">
+				Hello There!
 			</h1>
+			<p className="pt-4 pb-8">Welcome to my short presentation.</p>
 			<button className="bg-yellow-500 py-3 px-6 rounded-full transition duration-300 ease-in-out flex items-center animate-bounce">
 				Let's Go
 			</button>
