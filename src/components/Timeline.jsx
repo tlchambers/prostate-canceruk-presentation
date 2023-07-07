74cb7ffd8bc58b34b47cb2cbca7f9fffa639462d
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-07-07 23:48:08 +0100

    timeline code - not working, need debugging

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -4,9 +4,25 @@ import {
 	VerticalTimelineElement,
 } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component';
+import Journey from '../timelineElements';
 
 function Timeline() {
-	return <div>Timeline</div>;
+	return (
+		<div className="flex flex-col items-center justify-center h-screen">
+			<h1>Timeline</h1>
+			<VerticalTimeline>
+				{Journey.map((element) => {
+					return (
+						<VerticalTimelineElement
+							key={element.key}
+							date={element.date}
+							dateClassName="date"
+						></VerticalTimelineElement>
+					);
+				})}
+			</VerticalTimeline>
+		</div>
+	);
 }
 
 export default Timeline;
