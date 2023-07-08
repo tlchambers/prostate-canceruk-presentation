eb3935107d238fc7b3e1e7a41a5ef1e13ff3a7c7
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-07-08 12:23:45 +0100

    add smooth scroll to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-scroll';
 
 function Navbar() {
 	const [nav, setNav] = useState(false);
@@ -11,11 +11,27 @@ function Navbar() {
 	return (
 		<header className="fixed flex justify-between items-center w-full h-24 mx-auto px-8 shadow-sm bg-white z-10">
 			<h1 className="w-full text-3xl font-bold">TLC</h1>
-			<ul className="hidden md:flex uppercase text-sm">
-				<li className="p-4">Introduction</li>
-				<li className="p-4">About</li>
-				<li className="p-4 w-[145px]">Current SWOT</li>
-				<li className="p-4 w-[150px]">Future SWOT</li>
+			<ul className="hidden md:flex uppercase text-sm cursor-pointer">
+				<li className="p-4">
+					<Link to="introduction" smooth={true} duration={500}>
+						Introdution
+					</Link>
+				</li>
+				<li className="p-4">
+					<Link to="about" smooth={true} duration={500}>
+						About
+					</Link>
+				</li>
+				<li className="p-4 w-[145px]">
+					<Link to="current-swot" smooth={true} duration={500}>
+						Current SWOT
+					</Link>
+				</li>
+				<li className="p-4 w-[150px]">
+					<Link to="future-swot" smooth={true} duration={500}>
+						Future SWOT
+					</Link>
+				</li>
 			</ul>
 
 			<div onClick={handleNav} className="md:hidden">
@@ -29,10 +45,26 @@ function Navbar() {
 			>
 				<h1 className="p-6 w-full text-3xl font-bold text-center">TLC</h1>
 				<ul className="pt-4 uppercase ">
-					<li className="p-4 border-b border-gray-300">Introduction</li>
-					<li className="p-4 border-b border-gray-300">About</li>
-					<li className="p-4 border-b border-gray-300">Current SWOT</li>
-					<li className="p-4">Future SWOT</li>
+					<li className="p-4 border-b border-gray-300">
+						<Link to="introduction" smooth={true} duration={500}>
+							Introdution
+						</Link>
+					</li>
+					<li className="p-4 border-b border-gray-300">
+						<Link to="about" smooth={true} duration={500}>
+							About
+						</Link>
+					</li>
+					<li className="p-4 border-b border-gray-300">
+						<Link to="current-swot" smooth={true} duration={500}>
+							Current SWOT
+						</Link>
+					</li>
+					<li className="p-4">
+						<Link to="future-swot" smooth={true} duration={500}>
+							Future SWOT
+						</Link>
+					</li>
 				</ul>
 			</div>
 		</header>
