23499d8e3c35d6c12c5fcdd023f254de597006d5
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-07-10 16:43:37 +0100

    added name to make scroll work

--- src/components/Swot.jsx
+++ src/components/Swot.jsx
@@ -34,7 +34,10 @@ function Swot() {
 	};
 
 	return (
-		<div className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group">
+		<div
+			name="swot"
+			className="max-w-[1200px] h-[780px] w-full m-auto py-16 px-4 relative group"
+		>
 			<div
 				style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
 				className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
